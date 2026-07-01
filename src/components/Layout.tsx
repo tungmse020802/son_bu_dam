829c23030cf74fa611b29ae335a96d9703d17171
haul17 <longphhe140327@fpt.edu.vn>
2026-07-01 23:33:53 +0700

    fix_link_facebook

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -233,7 +233,7 @@ export function Footer() {
                   <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
                 </svg>
                 <span className="contact-text">
-                  <a href="[https://www.facebook.com/profile.php?id=61590118002475#](https://www.facebook.com/profile.php?id=61590118002475#)" target="_blank" rel="noreferrer">Fanpage: Sử Việt Anh Minh</a>
+                  <a href="https://www.facebook.com/profile.php?id=61590118002475#" target="_blank" rel="noreferrer">Fanpage: Sử Việt Anh Minh</a>
                 </span>
               </li>
               <li className="single-line-item">
